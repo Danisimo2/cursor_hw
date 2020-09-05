d895601d8e3d636d1b3d7c5ad5e7f5d34e83beee
Danylo <sukhiy37078@gmail.com>
2020-09-05 22:48:43 +0300

    Updates

---
+++ precache-manifest.00175b5aaa20672b04d85e5d2c9cfd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02694e32bb85162de924ff00cb708df0",
+    "url": "/cursor_hw/index.html"
+  },
+  {
+    "revision": "382e963ca2856046732f",
+    "url": "/cursor_hw/static/css/main.7b891105.chunk.css"
+  },
+  {
+    "revision": "14ab693299fa8b638e21",
+    "url": "/cursor_hw/static/js/2.c8fae3a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cursor_hw/static/js/2.c8fae3a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382e963ca2856046732f",
+    "url": "/cursor_hw/static/js/main.54966f2c.chunk.js"
+  },
+  {
+    "revision": "4d403eda26231dff2232",
+    "url": "/cursor_hw/static/js/runtime-main.7b72c204.js"
+  }
+]);
